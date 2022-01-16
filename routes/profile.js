6e2f3feabe148d9e9c02892d39fbfadba68db2e1
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-01-15 17:11:03 -0800

    GG

--- routes/profile.js
+++ routes/profile.js
@@ -37,8 +37,8 @@ router.delete("/delete/:user", (req, res) => {
    if(index == -1) {
        return res.send("Please input a valid user to delete!")
    }
-   database.splice(index,index)
-   username.splice(index, index)
+   database.splice(index,1)
+   username.splice(index, 1)
     res.status(201).send(`Successfully deleted ${req.params.user}.`)
 })
 
