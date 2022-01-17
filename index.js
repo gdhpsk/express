d4dd63805eb0c254141939ac22ded6c5960f4db5
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-01-16 21:13:48 -0800

    LOL

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ app.set("view engine", "ejs")
 
 app.use(logger)
 
-app.use(express.static("public"))
+app.use(express.static("public")) 
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 
-const port = process.env.PORT || 3000
-
-const levelsRouter = require("./routes/levels")
+const levelsRouter = require("./routes/levels") 
 const profileRouter = require("./routes/profile")
+const clicksRouter = require("./routes/clicks")
 app.use('/levels', levelsRouter)
 app.use('/profile', profileRouter)
+app.use("/clicks", clicksRouter)
 
 function logger(req, res, next) {
   console.log(`${req.method} ${req.originalUrl}`)
   next()
-} 
+}
 
-app.listen(port)
+app.listen(3000)
