d9865a57cb7034ad9ea3ec2a3732ab0d7d697497
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-01-16 21:14:54 -0800

    k

---
+++ routes/clicks.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const router = express.Router()
+const got = import("got")
+router.use(express.urlencoded({ extended: true }))
+const buttonclicks = require("../write.json")
+const fs = require("fs")
+
+router.get("/", (req, res) => {
+    res.render("button.ejs", {clicks: buttonclicks.numbers})
+})
+router.get("/json", (req, res) => {
+    res.json(buttonclicks)
+        const data = JSON.stringify({"numbers": buttonclicks.numbers+1});
+fs.writeFile('write.json', data, (err) => {
+    if (err) {
+        throw err;
+    }
+     console.log("JSON data is saved.");
+
+    })
+})
+module.exports = router
