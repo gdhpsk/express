e7a6ca00478158c2146b4e9d44e69638b2564495
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-01-24 21:44:50 -0800

    K

--- routes/clicks.js
+++ routes/clicks.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const got = import("got")
 router.use(express.urlencoded({ extended: true }))
 const fs = require("fs")
-const db = require("../schemas/clicks")
+const mongoose = require("mongoose")
+const db = mongoose.model("clicks")
 
 router.get("/", async (req, res) => {
     var buttonclicks = await db.findById("61ef1f0c789f36709356fd92")
